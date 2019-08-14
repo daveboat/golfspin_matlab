db6186372ccd6c2691f4f494328acccf238472f1
Dave Yan <daveyan80@gmail.com>
2019-08-14 17:37:00 -0400

    end of day commit, experimenting with tiled fft like in radar system

--- G_spin.m
+++ G_spin.m
@@ -1,11 +1,11 @@
 function [G]  = G_spin( t )
 %define parameters
 omega=733;%angular velocity in rad/s
-R=42e-3; %ball radius
+R=0.1;%42e-3; %ball radius
 E_0=1; %throwaway constant
 c=3e8; %speed of light
 f_0=300e6; %CW frequency
-v=120; %ball speed
+v=20; %ball speed
 d=5; %ball initial distance from radar
 
 G=@(theta,phi) R^2*sin(phi)*E_0 ...
--- main.m
+++ main.m
@@ -2,16 +2,17 @@ clc;clear;
 
 %hyperparameters
 t_end=50e-3;
-N=500;
+N=100;
 dt=t_end/N;
 n=1;
 G(n)=0;
 Gs(n)=0;
-t=0:dt:t_end;
-for tt = 0:dt:t_end
-    G(n) = integral2(G_nospin(tt),0,pi, 0,pi);
+t=0:dt:t_end*50;
+for tt = 0:dt:t_end*50
+    %G(n) = integral2(G_nospin(tt),0,pi, 0,pi);
     Gs(n)= integral2(G_spin(tt),0,pi, 0,pi);
     n=n+1;
+    display(sprintf('t = %f \n', tt));
 end
 
 %figure(1);title("Doppler signal");plot(t, G, 'r');
@@ -19,5 +20,5 @@ end
 
 %figure(3);title("Doppler signal");plot(t, Gs, 'g');
 %figure(4);title("FFT of doppler signal"); plot(abs(fftshift(fft(Gs))), 'g-');
-plot(abs(fftshift(fft(G))), 'r-');hold on;
-plot(abs(fftshift(fft(Gs))), 'g-'); 
+%plot(abs(fftshift(fft(G))), 'r-');hold on;
+%plot(abs(fftshift(fft(Gs))), 'g-'); 
